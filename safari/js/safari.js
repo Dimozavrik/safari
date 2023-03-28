13c451b229709eccd671c1d7cb28b227d7964b68
Cheshev Dmitry <109523341+Dimozavrik@users.noreply.github.com>
2023-03-28 12:11:51 +0300

    Add files via upload

---
+++ safari/js/safari.js
@@ -0,0 +1,30 @@
+$ (function() {
+
+  $('.popup__link').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+
+  $('.gallery__slider').slick({
+    fade: true,
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt="prev"></button>', 
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt="next"></button>', 
+  });
+
+  $('.gallery__item-inner').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+  });
+
+});
